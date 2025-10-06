e5eb8ddcdddb4f92618392d294541b904acf732f
Shawn Zhong <github@shawnzhong.com>
2025-10-06 13:47:35 -0500

    Control sched clock (#6)
    * Update config
    
    * Update
    
    * Update
    
    * Update
    
    * Update

--- kmod/main.c
+++ kmod/main.c
@@ -6,25 +6,40 @@
 
 #define KERNEL_SYMBOL_LIST                                                     \
   X(void, tick_sched_timer_dying, (int cpu))                                   \
-  X(void, sched_tick, (void))
-
+  X(void, sched_tick, (void))                                                  \
+  X(u64, sched_clock, (void))                                                  \
+  X(void, paravirt_set_sched_clock, (u64(*func)(void)))                        \
+  X(u64, kvm_sched_clock_read, (void))                                         \
+  X(void, clear_sched_clock_stable, (void))
 #include "kernel_sym.h"
 #include "logging.h"
+
+#define TARGET_CPU 1
+#define NS_PER_MS 1000000
+#define KTHREAD_SLEEP_MS 10000
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Shawn Zhong");
 MODULE_DESCRIPTION("Scheduler control");
 
-static void main_remote(void *data) {
-  TRACE_INFO("sched_tick initiated on CPU %d: current->comm=%s\n",
-             smp_processor_id(), current->comm);
+static u64 mocked_sched_clock_value = 0;
+static u64 mocked_sched_clock(void) {
+  if (smp_processor_id() == 0)
+    return kernel_kvm_sched_clock_read();
+  return mocked_sched_clock_value;
+}
+
+static void remote_fn(void *data) {
+  TRACE_INFO("sched_tick initiated on CPU %d: comm=%s, clock=%llu\n",
+             smp_processor_id(), current->comm, kernel_sched_clock());
+  mocked_sched_clock_value += NS_PER_MS;
   kernel_sched_tick();
 }
 
 static int main_kthread(void *data) {
   while (1) {
-    msleep(3000);
-    smp_call_function_single(1, main_remote, NULL, 0);
+    msleep(KTHREAD_SLEEP_MS);
+    smp_call_function_single(TARGET_CPU, remote_fn, NULL, 0);
   }
   return 0;
 }
@@ -37,6 +52,14 @@ static void sched_tick_callback(unsigned long ip, unsigned long parent_ip,
   TRACE_INFO("sched_tick called on CPU %d\n", smp_processor_id());
 }
 
+static void update_rq_clock_callback(unsigned long ip, unsigned long parent_ip,
+                                     struct ftrace_ops *op,
+                                     struct ftrace_regs *fregs) {
+  if (smp_processor_id() == 0)
+    return;
+  TRACE_INFO("update_rq_clock called on CPU %d\n", smp_processor_id());
+}
+
 static int __init init(void) {
   if (smp_processor_id() != 0) {
     TRACE_ERROR("Current CPU %d is not 0\n", smp_processor_id());
@@ -44,15 +67,31 @@ static int __init init(void) {
   }
   init_kernel_symbols();
 
-  TRACE_INFO("Freezing CPU %d\n", 1);
-  kernel_tick_sched_timer_dying(1);
+  TRACE_INFO("Freezing CPU %d\n", TARGET_CPU);
+  kernel_tick_sched_timer_dying(TARGET_CPU);
+  kernel_clear_sched_clock_stable();
+
+  mocked_sched_clock_value =
+      kernel_kvm_sched_clock_read() / NS_PER_MS * NS_PER_MS;
+  kernel_paravirt_set_sched_clock(mocked_sched_clock);
 
   // Initialize ftrace
-  struct ftrace_ops ftrace_ops = {.func = &sched_tick_callback};
-  ftrace_set_filter(&ftrace_ops, "sched_tick", strlen("sched_tick"), 1);
-  if (register_ftrace_function(&ftrace_ops)) {
-    TRACE_ERROR("Failed to initialize ftrace\n");
-    return -EINVAL;
+  {
+    struct ftrace_ops ftrace_ops = {.func = &sched_tick_callback};
+    ftrace_set_filter(&ftrace_ops, "sched_tick", strlen("sched_tick"), 1);
+    if (register_ftrace_function(&ftrace_ops)) {
+      TRACE_ERROR("Failed to initialize ftrace\n");
+      return -EINVAL;
+    }
+  }
+  {
+    struct ftrace_ops ftrace_ops = {.func = &update_rq_clock_callback};
+    ftrace_set_filter(&ftrace_ops, "update_rq_clock", strlen("update_rq_clock"),
+                      1);
+    if (register_ftrace_function(&ftrace_ops)) {
+      TRACE_ERROR("Failed to initialize ftrace\n");
+      return -EINVAL;
+    }
   }
 
   // Create kthread
