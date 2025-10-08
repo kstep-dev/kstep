085969efc5b4fa3d05ec31e4538d275a0f39edbf
Shawn Zhong <github@shawnzhong.com>
2025-10-08 02:50:09 -0500

    Remove UML (#9)
    * Remove UML
    
    * Update
    
    * Update
    
    * Update

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 SchedTest
 ==============
 
-- Download Linux kernel: `./clone_linux.py`
+- Download Linux kernel: `./fetch_linux.py [--versions <version>...] [--tarball]`
 
 - Install dependencies: `./install_deps.sh`
 
-- Build Linux kernel: `./make_linux.py [--uml] [--clean]`
+- Build Linux kernel: `./make_linux.py [--clean]`
 
-- Prepare root filesystem: `make` (triggered by `./run_*.py` as well)
+- Prepare root filesystem: `make` (also triggered by `./run_*.py`)
 
-- Run kernel: `./run_qemu.py [--debug]` or `./run_uml.py`
+- Run kernel: `./run_qemu.py [--debug]`
+
+- Debug kernel: `./run_gdb.py`
--- make_linux.py
+++ make_linux.py
@@ -6,27 +6,24 @@ from scripts import LINUX_CUSTOM_CONFIG, LINUX_DIR, system
 
 BEAR_CMD = "bear --append --output compile_commands.json --"
 
-def make_linux(uml: bool = False, clean: bool = False):
-    extra = " ARCH=um" if uml else ""
-
+def make_linux(clean: bool = False):
     # Clean up old build
     if clean:
         system(f"make -C {LINUX_DIR} -j$(nproc) mrproper")
 
     # Generate config
     if not (LINUX_DIR / ".config").exists():
-        system(f"make -C {LINUX_DIR} -j$(nproc) defconfig {extra}")
+        system(f"make -C {LINUX_DIR} -j$(nproc) defconfig")
         system(
             f"cd {LINUX_DIR} && {LINUX_DIR}/scripts/kconfig/merge_config.sh {LINUX_DIR}/.config {LINUX_CUSTOM_CONFIG}"
         )
 
     # Build kernel
-    system(f"{BEAR_CMD} make -C {LINUX_DIR} -j$(nproc) {extra}")
+    system(f"{BEAR_CMD} make -C {LINUX_DIR} -j$(nproc)")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--uml", action=argparse.BooleanOptionalAction, default=False)
     parser.add_argument("--clean", action=argparse.BooleanOptionalAction, default=False)
     args = parser.parse_args()
     make_linux(**vars(args))
