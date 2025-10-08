71c21dee32f9b9f57c0d3f135920cfef0de541de
ShawnZhong <github@shawnzhong.com>
2025-10-08 02:26:25 -0500

    Update

--- clone_linux.py
+++ clone_linux.py
@@ -16,23 +16,46 @@ def clone_master():
     system(f"git clone {LINUX_GIT_URL} {LINUX_MASTER_DIR}")
 
 
-def add_worktree(version: str, current: bool = False):
-    worktree_dir = LINUX_VERSIONS_DIR / version
+def add_worktree(version: str):
+    worktree_dir = LINUX_VERSIONS_DIR / f"linux-{version}-git"
     if worktree_dir.exists():
         logging.info(f"Linux {version} already cloned to {worktree_dir}")
-    else:
-        system(f"cd {LINUX_MASTER_DIR} && git worktree add {worktree_dir} {version}")
+        return
+    system(f"cd {LINUX_MASTER_DIR} && git worktree add {worktree_dir} {version}")
+    return worktree_dir
 
-    if current:
-        LINUX_DIR.unlink(missing_ok=True)
-        LINUX_DIR.symlink_to(worktree_dir)
+
+def download_tarball(version: str):
+    tarball_url = f"https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-{version}.tar.xz"
+    tarball_path = LINUX_VERSIONS_DIR / f"{version}.tar.xz"
+    if tarball_path.exists():
+        logging.info(f"Linux {version} tarball already downloaded to {tarball_path}")
+    else:
+        system(f"wget {tarball_url} -O {tarball_path}")
+
+    src_path = LINUX_VERSIONS_DIR / f"linux-{version}"
+    if src_path.exists():
+        logging.info(f"Linux {version} source already extracted to {src_path}")
+    else:
+        system(f"tar -xvf {tarball_path} -C {LINUX_VERSIONS_DIR}")
+    return src_path
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--versions", nargs="+", type=str, default=["v6.14"])
+    parser.add_argument("--versions", nargs="+", type=str, default=["6.14"])
+    parser.add_argument("--tarball", help="Download tarball", action="store_true")
     args = parser.parse_args()
 
-    clone_master()
+    if not args.tarball:
+        clone_master()
+
     for version in args.versions:
-        add_worktree(version, current=version == args.versions[0])
+        if args.tarball:
+            path = download_tarball(version)
+        else:
+            path = add_worktree(version)
+
+        if version == args.versions[0]:
+            LINUX_DIR.unlink(missing_ok=True)
+            LINUX_DIR.symlink_to(path)
