ccfed22d55724e5ac5b57b655e893dd6f79d1f8a
ShawnZhong <github@shawnzhong.com>
2025-10-10 01:00:04 -0500

    Update

--- make_linux.py
+++ make_linux.py
@@ -7,7 +7,7 @@ from scripts import LINUX_CONFIG, get_linux_dir, system
 
 BEAR_CMD = "bear --append --output compile_commands.json --"
 
-def make_linux(linux_dir: Path, clean: bool = False):
+def make_linux(linux_dir: Path, clean: bool = False, modules_prepare: bool = False):
     # Clean up old build
     if clean:
         system(f"make -C {linux_dir} -j$(nproc) mrproper")
@@ -20,13 +20,21 @@ def make_linux(linux_dir: Path, clean: bool = False):
         )
 
     # Build kernel
-    system(f"{BEAR_CMD} make -C {linux_dir} -j$(nproc)")
+    cmd = f"{BEAR_CMD} make -C {linux_dir} -j$(nproc)"
+    if modules_prepare:
+        cmd += " modules_prepare"
+    system(cmd)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--clean", action=argparse.BooleanOptionalAction, default=False)
     parser.add_argument("--versions", nargs="+", type=str, default=["6.14"])
+    parser.add_argument("--modules_prepare", action="store_true", default=False)
     args = parser.parse_args()
     for version in args.versions:
-        make_linux(linux_dir=get_linux_dir(version), clean=args.clean)
+        make_linux(
+            linux_dir=get_linux_dir(version),
+            clean=args.clean,
+            modules_prepare=args.modules_prepare,
+        )
