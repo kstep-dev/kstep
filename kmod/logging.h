f469886c8cfd587aed55e142ab7ef16424c1e7dc
ShawnZhong <github@shawnzhong.com>
2025-10-10 00:46:42 -0500

    Update

--- kmod/logging.h
+++ kmod/logging.h
@@ -16,21 +16,19 @@
 #define TERM_CYAN "\033[96m"
 #define TERM_RESET "\033[0m"
 
-#define TERM_COLOR_DEBUG TERM_GRAY
+#define TERM_COLOR_DEBUG TERM_BLUE
 #define TERM_COLOR_INFO TERM_GREEN
 #define TERM_COLOR_ERR TERM_RED
 
 #define TRACE_PRINT(level, fmt, ...)                                           \
   (void)(LOGLEVEL_##level <= TRACE_LEVEL &&                                    \
-         (printk(KERN_##level TERM_COLOR_##level                               \
-                 "[%12s:%-3d] %16s: " fmt TERM_RESET,                          \
+         (printk(KERN_INFO TERM_COLOR_##level                                  \
+                 "[%12s:%-3d] %16s: " fmt TERM_RESET "\n",                     \
                  __FILE_NAME__, __LINE__, __func__, ##__VA_ARGS__),            \
           0))
 
 #define TRACE_DEBUG(fmt, ...) TRACE_PRINT(DEBUG, fmt, ##__VA_ARGS__)
 #define TRACE_INFO(fmt, ...) TRACE_PRINT(INFO, fmt, ##__VA_ARGS__)
 #define TRACE_ERR(fmt, ...) TRACE_PRINT(ERR, fmt, ##__VA_ARGS__)
-
-#define TRACE_CONT(fmt, ...) printk(KERN_CONT fmt, ##__VA_ARGS__)
 
 #endif
