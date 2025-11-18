952e083e5b5ffe882ca7b6c35a891bd5a0f9e8c2
Shawn Zhong <github@shawnzhong.com>
2025-11-18 03:49:04 -0600

    Refactor ksym (#74)

--- kmod/ksym.c
+++ kmod/ksym.c
@@ -1,10 +1,9 @@
-#include "ksym.h"
-
 #include <linux/kprobes.h>
 
-#include "logging.h"
+#include "kstep.h"
 
 struct ksym_t ksym;
+static void *(*kallsyms_lookup_name_fn)(const char *name);
 
 static void *ksym_get_kallsyms_lookup_name(void) {
   struct kprobe kp = {.symbol_name = "kallsyms_lookup_name"};
@@ -19,9 +18,9 @@ static void *ksym_get_kallsyms_lookup_name(void) {
 }
 
 static void *ksym_get_addr(const char *name, void *default_addr) {
-  void *addr = (void *)ksym.kallsyms_lookup_name(name);
+  void *addr = (void *)kallsyms_lookup_name_fn(name);
   if (addr == NULL) {
-    TRACE_INFO("Symbol %s not found", name);
+    TRACE_ERR("Symbol %s not found", name);
     return default_addr;
   } else {
     TRACE_DEBUG("Symbol %-32s -> %px", name, addr);
@@ -29,24 +28,25 @@ static void *ksym_get_addr(const char *name, void *default_addr) {
   }
 }
 
-#define X(type, name, ...)                                                     \
+#define KSYM_VAR(type, name)
+#define KSYM_FUNC(type, name, ...)                                             \
   static void stub_##name(void) {                                              \
     panic("Uninitialized ksym " #name " called");                              \
   }
-KSYM_FUNC_LIST
-#undef X
+#include "ksym.h"
+#undef KSYM_FUNC
+#undef KSYM_VAR
 
 void ksym_init(void) {
-  ksym.kallsyms_lookup_name = ksym_get_kallsyms_lookup_name();
+  kallsyms_lookup_name_fn = ksym_get_kallsyms_lookup_name();
 
-#define X(type, name, ...)                                                     \
+#define KSYM_FUNC(type, name, ...)                                             \
   ksym.name = ksym_get_addr(#name, (void *)&stub_##name);
-  KSYM_FUNC_LIST
-#undef X
-
-#define X(type, name) ksym.name = ksym_get_addr(#name, (void *)0xdeadbeef);
-  KSYM_VAR_LIST
-#undef X
+#define KSYM_VAR(type, name)                                                   \
+  ksym.name = ksym_get_addr(#name, (void *)0xdeadbeef);
+#include "ksym.h"
+#undef KSYM_FUNC
+#undef KSYM_VAR
 
   TRACE_INFO("ksym initialized");
 }
