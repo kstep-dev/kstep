82bc27dd20ad8de974f458fc6894edfc74d756e5
Shawn Zhong <github@shawnzhong.com>
2025-12-04 08:59:22 +0000

    Add cpio to deps

--- install_deps.sh
+++ install_deps.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
 sudo apt update
-sudo apt install -y build-essential flex bison bc libncurses-dev libssl-dev libelf-dev bear qemu-kvm gdb
+sudo apt install -y build-essential flex bison bc libncurses-dev libssl-dev libelf-dev bear qemu-kvm gdb cpio
 
 # Install uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
