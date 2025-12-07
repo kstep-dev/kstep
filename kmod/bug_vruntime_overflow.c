fe81ae5c37e65659b2e8da50c119aa98b6727929
Shawn Zhong <github@shawnzhong.com>
2025-12-06 20:32:53 -0600

    Update plot (#132)

--- kmod/bug_vruntime_overflow.c
+++ kmod/bug_vruntime_overflow.c
@@ -1,49 +1,49 @@
 #include "kstep.h"
 
-static struct task_struct *tasks[4];
+static struct task_struct *spcial_task;
+static struct task_struct *starved_task;
+static struct task_struct *other_task;
 
 static void init(void) {
-  // Setup cgroups
   kstep_cgroup_create_pinned("g0", "1");
   kstep_cgroup_create_pinned("g1", "1");
-  kstep_cgroup_set_weight("g0", 20);
+  kstep_cgroup_set_weight("g0", 15);
+  kstep_cgroup_set_weight("g1", 15);
 
-  // 2 tasks in each group
-  for (int i = 0; i < ARRAY_SIZE(tasks); i++) {
-    tasks[i] = kstep_task_create();
-    kstep_cgroup_add_task(i < 2 ? "g0" : "g1", tasks[i]->pid);
-  }
+  spcial_task = kstep_task_create();
+  starved_task = kstep_task_create();
+  other_task = kstep_task_create();
+
+  kstep_cgroup_add_task("g0", spcial_task->pid);
+  kstep_cgroup_add_task("g0", starved_task->pid);
+  kstep_cgroup_add_task("g1", other_task->pid);
 }
 
 static void *ineligible_group_with_eligible_tasks(void) {
-  struct task_struct *p = tasks[0];
-  if (p->on_cpu && !kstep_eligible(p->se.parent) && kstep_eligible(&p->se))
-    return p;
+  if (spcial_task->on_cpu && !kstep_eligible(spcial_task->se.parent) &&
+      kstep_eligible(&spcial_task->se))
+    return spcial_task;
   return NULL;
 }
 
 static void body(void) {
-  // Wake up all tasks except for tasks[1]
-  for (int i = 0; i < ARRAY_SIZE(tasks); i++)
-    if (i != 1)
-      kstep_task_wakeup(tasks[i]);
+  kstep_task_wakeup(spcial_task);
+  kstep_task_wakeup(other_task);
 
   kstep_tick_repeat(10);
-
   kstep_tick_until(ineligible_group_with_eligible_tasks);
 
   // Pause ineligible task
-  kstep_task_pause(tasks[0]);
+  kstep_task_pause(spcial_task);
 
   // Dequeue ineligible group
-  struct sched_entity *group_se = tasks[0]->se.parent;
+  struct sched_entity *group_se = spcial_task->se.parent;
   ksym.dequeue_entities(group_se->cfs_rq, group_se, DEQUEUE_SLEEP);
 
   // Reweight the group
-  kstep_cgroup_set_weight("g0", 100);
+  kstep_cgroup_set_weight("g0", 16);
 
-  // Starvation for tasks[1]
-  kstep_task_wakeup(tasks[1]);
+  kstep_task_wakeup(starved_task);
 
   kstep_tick_repeat(18);
 }
