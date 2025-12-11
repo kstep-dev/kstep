d70f888771755bf693121109fc5aaa8e70f8f510
Shawn Zhong <github@shawnzhong.com>
2025-12-10 20:11:12 -0600

    Remove unused ksym (#146)
    * Remove unused ksym
    
    * Update
    
    * Update

--- kmod/ksym.h
+++ kmod/ksym.h
@@ -1,15 +1,17 @@
 #include <linux/version.h>
 
-#ifndef KSYM_FUNC
-#define KSYM_FUNC(...)
-#endif
-
-#ifndef KSYM_VAR
-#define KSYM_VAR(...)
-#endif
-
 // Define function symbols
 // KSYM_FUNC(ret_type, func_name, arg...) declares function `ksym.func_name`
+#ifndef KSYM_FUNC
+#define KSYM_FUNC(ret_type, func_name, arg...)
+#endif
+
+// Define variable symbols
+// KSYM_VAR(type, name) declares `ksym.name` as a *pointer* to the variable
+#ifndef KSYM_VAR
+#define KSYM_VAR(type, name)
+#endif
+
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 10, 0)
 KSYM_FUNC(void, sched_tick, void)
 #else
@@ -21,7 +23,6 @@ KSYM_FUNC(void, tick_setup_sched_timer, bool hrtimer)
 KSYM_FUNC(int, workqueue_offline_cpu, int cpu)
 KSYM_FUNC(void, update_rq_clock, struct rq *rq)
 KSYM_FUNC(int, entity_eligible, struct cfs_rq *cfs_rq, struct sched_entity *se)
-KSYM_FUNC(void, signal_wake_up_state, struct task_struct *t, int state)
 KSYM_FUNC(int, try_to_wake_up, struct task_struct *p, unsigned int state,
           int wake_flags)
 KSYM_FUNC(void, freeze_task, struct task_struct *p)
@@ -35,8 +36,6 @@ KSYM_FUNC(void, arch_set_cpu_capacity, int cpu, unsigned long cap,
           unsigned long max_cap, unsigned long cap_freq,
           unsigned long base_freq)
 
-// Define variable symbols
-// KSYM_VAR(type, name) declares `ksym.name` as a *pointer* to the variable
 KSYM_VAR(struct rq, runqueues)
 #ifdef CONFIG_GENERIC_SCHED_CLOCK
 KSYM_VAR(void, cd)
@@ -45,7 +44,6 @@ KSYM_VAR(u64, __sched_clock_offset)
 KSYM_VAR(unsigned int, sysctl_sched_migration_cost)
 KSYM_VAR(bool, pm_freezing)
 KSYM_VAR(unsigned long, arch_freq_scale)
-KSYM_VAR(const struct sched_class, rt_sched_class)
 KSYM_VAR(int, tick_do_timer_cpu)
 KSYM_VAR(ktime_t, tick_next_period)
 KSYM_VAR(int, distribute_cpu_mask_prev)
