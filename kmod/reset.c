b12d5fa9c29692269baaf27a222a5920e74596a1
ShawnZhong <github@shawnzhong.com>
2025-12-12 19:36:55 -0600

    Move kstep_patch_min_vruntime to reset.c

--- kmod/reset.c
+++ kmod/reset.c
@@ -24,6 +24,14 @@ void kstep_reset_task(struct task_struct *p) {
   p->se.avg.load_avg = scale_load_down(p->se.load.weight);
 }
 
+static void kstep_reset_tasks(void) {
+  struct task_struct *p;
+  for_each_process(p) {
+    if (task_cpu(p) != 0)
+      kstep_reset_task(p);
+  }
+}
+
 static void kstep_reset_rq(void) {
   for (int cpu = 1; cpu < num_online_cpus(); cpu++) {
     struct rq *rq = cpu_rq(cpu);
@@ -73,13 +81,34 @@ static void kstep_reset_cpumask(void) {
 #endif
 }
 
+// Set min vruntime for newly created cfs_rq
+static void set_min_vruntime(unsigned long ip, unsigned long parent_ip,
+                             struct ftrace_ops *op, struct ftrace_regs *fregs) {
+  struct cfs_rq *cfs_rq = (void *)regs_get_kernel_argument((void *)fregs, 1);
+  cfs_rq->min_vruntime = INIT_TIME_NS;
+  TRACE_INFO("Set min vruntime to %llu ns", INIT_TIME_NS);
+}
+
+struct ftrace_ops set_min_vruntime_op = {
+    .func = set_min_vruntime,
+    .flags = FTRACE_OPS_FL_SAVE_REGS_IF_SUPPORTED | FTRACE_OPS_FL_RECURSION,
+};
+
+void kstep_patch_min_vruntime(void) {
+  char *name = "init_tg_cfs_entry";
+  if (ftrace_set_filter(&set_min_vruntime_op, name, strlen(name), 1))
+    panic("Failed to set filter for %s", name);
+
+  if (register_ftrace_function(&set_min_vruntime_op))
+    panic("Failed to register ftrace function for %s", name);
+
+  TRACE_INFO("Patched %s to set min vruntime", name);
+}
+
 void kstep_reset_sched(void) {
   kstep_reset_rq();
   kstep_reset_cpumask();
-  struct task_struct *p;
-  for_each_process(p) {
-    if (task_cpu(p) != 0)
-      kstep_reset_task(p);
-  }
+  kstep_reset_tasks();
+  kstep_patch_min_vruntime();
   TRACE_INFO("Reset scheduler state");
 }
