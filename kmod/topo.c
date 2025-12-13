5eace7ec1b35923414405094a665346ad5443df9
Shawn Zhong <github@shawnzhong.com>
2025-12-13 17:11:20 -0600

    Improve topo api (#153)
    * Improve topo api
    
    * Update
    
    * Update
    
    * Update
    
    * Update

--- kmod/topo.c
+++ kmod/topo.c
@@ -21,9 +21,10 @@ static void print_cpumask(const struct cpumask *mask, int width) {
 static void print_topo_levels(void) {
   pr_info("Topology levels:\n");
   struct sched_domain_topology_level *tl;
+  int nr_cpus = num_online_cpus();
   for_each_tl(tl) {
     pr_info("- %-5s| ", tl->name);
-    for (int cpu = 0; cpu < num_online_cpus(); cpu++) {
+    for (int cpu = 0; cpu < nr_cpus; cpu++) {
 // https://github.com/torvalds/linux/commit/661f951e371cc134ea31c84238dbdc9a898b8403
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 18, 0)
       const struct cpumask *mask = tl->mask(tl, cpu);
@@ -56,8 +57,9 @@ static void print_sched_domain(struct sched_domain *sd) {
 static void print_sched_domains(void) {
   pr_info("Sched domains:\n");
   struct sched_domain_topology_level *tl;
+  int nr_cpus = num_online_cpus();
   for_each_tl(tl) {
-    for (int cpu = 0; cpu < num_online_cpus(); cpu++) {
+    for (int cpu = 0; cpu < nr_cpus; cpu++) {
       struct sched_domain *sd;
       for_each_domain(cpu, sd) {
         if (strcmp(sd->name, tl->name) != 0)
@@ -74,7 +76,82 @@ void kstep_topo_print(void) {
   print_sched_domains();
 }
 
-static void kstep_topo_apply(void) {
+enum kstep_topo_type {
+  KSTEP_TOPO_SMT,
+  KSTEP_TOPO_CLS,
+  KSTEP_TOPO_MC,
+  KSTEP_TOPO_PKG,
+  KSTEP_TOPO_NODE,
+  KSTEP_TOPO_NR,
+};
+
+static struct cpumask kstep_masks[KSTEP_TOPO_NR][NR_CPUS];
+
+// https://github.com/torvalds/linux/commit/661f951e371cc134ea31c84238dbdc9a898b8403
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 18, 0)
+#define DEFINE_MASKS_FN(name, type)                                            \
+  static const struct cpumask *name(struct sched_domain_topology_level *tl,    \
+                                    int cpu) {                                 \
+    return &kstep_masks[type][cpu];                                            \
+  }
+#else
+#define DEFINE_MASKS_FN(name, type)                                            \
+  static const struct cpumask *name(int cpu) { return &kstep_masks[type][cpu]; }
+#endif
+
+DEFINE_MASKS_FN(smt_masks_fn, KSTEP_TOPO_SMT)
+DEFINE_MASKS_FN(cls_masks_fn, KSTEP_TOPO_CLS)
+DEFINE_MASKS_FN(mc_masks_fn, KSTEP_TOPO_MC)
+DEFINE_MASKS_FN(pkg_masks_fn, KSTEP_TOPO_PKG)
+DEFINE_MASKS_FN(node_masks_fn, KSTEP_TOPO_NODE)
+
+static sched_domain_mask_f kstep_masks_fns[KSTEP_TOPO_NR] = {
+    [KSTEP_TOPO_SMT] = smt_masks_fn,   [KSTEP_TOPO_CLS] = cls_masks_fn,
+    [KSTEP_TOPO_MC] = mc_masks_fn,     [KSTEP_TOPO_PKG] = pkg_masks_fn,
+    [KSTEP_TOPO_NODE] = node_masks_fn,
+};
+
+static enum kstep_topo_type get_topo_type(const char *name) {
+  if (strcmp(name, "SMT") == 0)
+    return KSTEP_TOPO_SMT;
+  if (strcmp(name, "CLS") == 0)
+    return KSTEP_TOPO_CLS;
+  if (strcmp(name, "MC") == 0)
+    return KSTEP_TOPO_MC;
+  // https://github.com/torvalds/linux/commit/f577cd57bfaa889cf0718e30e92c08c7f78c9d85
+  if (strcmp(name, "PKG") == 0 || strcmp(name, "DIE") == 0)
+    return KSTEP_TOPO_PKG;
+  if (strcmp(name, "NODE") == 0)
+    return KSTEP_TOPO_NODE;
+  panic("Unknown topology type %s", name);
+}
+
+void kstep_topo_init(void) {
+  int nr_cpus = num_online_cpus();
+  struct sched_domain_topology_level *tl;
+  for_each_tl(tl) {
+    for (int cpu = 0; cpu < nr_cpus; cpu++) {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 18, 0)
+      const struct cpumask *old_mask = tl->mask(tl, cpu);
+#else
+      const struct cpumask *old_mask = tl->mask(cpu);
+#endif
+      cpumask_copy(&kstep_masks[get_topo_type(tl->name)][cpu], old_mask);
+    }
+    tl->mask = kstep_masks_fns[get_topo_type(tl->name)];
+  }
+}
+
+void kstep_topo_set_level(enum kstep_topo_type type, const char *cpulists[],
+                          int size) {
+  if (size != num_online_cpus())
+    panic("size %d != num_online_cpus %d", size, num_online_cpus());
+  for (int i = 0; i < size; i++)
+    if (cpulist_parse(cpulists[i], &kstep_masks[type][i]) < 0)
+      panic("Failed to parse cpulist %s for level %d", cpulists[i], type);
+}
+
+void kstep_topo_apply(void) {
 #ifdef CONFIG_GENERIC_ARCH_TOPOLOGY
   // https://elixir.bootlin.com/linux/v6.17.8/source/drivers/base/arch_topology.c#L205-L222
   *ksym.update_topology = 1;
@@ -85,6 +162,19 @@ static void kstep_topo_apply(void) {
 #endif
 
   ksym.rebuild_sched_domains();
+}
+
+void kstep_topo_use_special(void) {
+  int nr_cpus = num_online_cpus();
+  for (int cpu = 0; cpu < nr_cpus; cpu++)
+    kstep_cpu_set_capacity(cpu, (cpu % 2 == 0) ? SCHED_CAPACITY_SCALE
+                                               : SCHED_CAPACITY_SCALE / 2);
+
+  kstep_topo_init();
+  const char *cpulists[] = {"0-1", "0-1", "2-3", "2-3",
+                            "4-5", "4-5", "6-7", "6-7"};
+  kstep_topo_set_level(KSTEP_TOPO_CLS, cpulists, ARRAY_SIZE(cpulists));
+  kstep_topo_apply();
 }
 
 void kstep_cpu_set_freq(int cpu, int scale) {
@@ -110,46 +200,4 @@ void kstep_cpu_set_capacity(int cpu, int scale) {
   ksym.arch_set_cpu_capacity(cpu, scale, SCHED_CAPACITY_SCALE, scale,
                              SCHED_CAPACITY_SCALE);
 #endif
-}
-
-static void parse_cpumasks(int count, const char *cpulists[],
-                           struct cpumask masks[]) {
-  if (count != num_online_cpus()) {
-    panic(
-        "Number of CPUs in cpulists %d does not match number of online CPUs %d",
-        count, num_online_cpus());
-  }
-  for (int i = 0; i < count; i++) {
-    if (cpulist_parse(cpulists[i], &masks[i]) < 0) {
-      panic("Failed to parse cpulist %s", cpulists[i]);
-    }
-  }
-}
-
-static struct cpumask cluster_masks[NR_CPUS];
-static const struct cpumask *cluster_masks_fn(
-// https://github.com/torvalds/linux/commit/661f951e371cc134ea31c84238dbdc9a898b8403
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 18, 0)
-    struct sched_domain_topology_level *tl,
-#endif
-    int cpu) {
-  return &cluster_masks[cpu];
-}
-
-void kstep_topo_use_special(void) {
-  for (int cpu = 0; cpu < num_online_cpus(); cpu++) {
-    kstep_cpu_set_capacity(cpu, (cpu % 2 == 0) ? SCHED_CAPACITY_SCALE
-                                               : SCHED_CAPACITY_SCALE >> 1);
-  }
-
-  // qemu-system-x86_64 does not support cluster CPU topology.
-  const char *cpulists[] = {"0-1", "0-1", "2-3", "2-3",
-                            "4-5", "4-5", "6-7", "6-7"};
-  parse_cpumasks(ARRAY_SIZE(cpulists), cpulists, cluster_masks);
-  struct sched_domain_topology_level *tl;
-  for_each_tl(tl) {
-    if (strcmp(tl->name, "CLS") == 0)
-      tl->mask = cluster_masks_fn;
-  }
-  kstep_topo_apply();
 }
