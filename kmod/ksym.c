6b4d3a89157bc8d21b394df16d2f7881e26003b7
ShawnZhong <github@shawnzhong.com>
2025-12-16 01:18:02 -0600

    Simplify logging

--- kmod/ksym.c
+++ kmod/ksym.c
@@ -8,11 +8,9 @@ static void *(*kallsyms_lookup_name_fn)(const char *name);
 static void *ksym_get_kallsyms_lookup_name(void) {
   struct kprobe kp = {.symbol_name = "kallsyms_lookup_name"};
   int ret = register_kprobe(&kp);
-  if (ret < 0) {
-    TRACE_ERR("Failed to register kprobe: %d. Using hardcoded address.", ret);
-    // From `nm linux/current/linux | grep kallsyms_lookup_name`
-    return (void *)0x600ad515;
-  }
+  if (ret < 0) 
+    panic("Failed to register kprobe: %d", ret);
+  
   unregister_kprobe(&kp);
   return (void *)kp.addr;
 }
@@ -20,12 +18,10 @@ static void *ksym_get_kallsyms_lookup_name(void) {
 static void *ksym_get_addr(const char *name, void *default_addr) {
   void *addr = (void *)kallsyms_lookup_name_fn(name);
   if (addr == NULL) {
-    TRACE_ERR("Symbol %s not found", name);
+    TRACE_INFO("Symbol %s not found", name);
     return default_addr;
-  } else {
-    TRACE_DEBUG("Symbol %-32s -> %px", name, addr);
-    return addr;
   }
+  return addr;
 }
 
 #define KSYM_VAR(type, name)
@@ -38,6 +34,7 @@ static void *ksym_get_addr(const char *name, void *default_addr) {
 #undef KSYM_VAR
 
 void ksym_init(void) {
+  TRACE_INFO("Initializing ksym");
   kallsyms_lookup_name_fn = ksym_get_kallsyms_lookup_name();
 
 #define KSYM_FUNC(type, name, ...)                                             \
@@ -47,6 +44,4 @@ void ksym_init(void) {
 #include "ksym.h"
 #undef KSYM_FUNC
 #undef KSYM_VAR
-
-  TRACE_INFO("ksym initialized");
 }
