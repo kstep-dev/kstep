f4f7d220ec47f29db5928dd0ac20c354ae81ae2f
Shawn Zhong <github@shawnzhong.com>
2026-01-21 16:00:47 -0600

    Add all png plots (#181)
    * Add all png plots
    
    * Update

--- scripts/plot_util_avg.py
+++ scripts/plot_util_avg.py
@@ -8,6 +8,7 @@ import argparse
 import matplotlib.pyplot as plt
 from consts import RESULTS_DIR
 from parse import parse_rq
+from plot_utils import save_fig
 
 
 def parse_log_file(log_file):
@@ -15,57 +16,71 @@ def parse_log_file(log_file):
     return df["timestamp"], df["avg_util"]
 
 
-def plot_util_avg(timestamps_buggy, util_avg_values_buggy, 
-                  timestamps_fixed, util_avg_values_fixed, bugId, output_file):
+def plot_util_avg(
+    timestamps_buggy,
+    util_avg_values_buggy,
+    timestamps_fixed,
+    util_avg_values_fixed,
+):
     """
     Plot util_avg over time in two subplots
     """
-    fig, (ax1, ax2) = plt.subplots(2, 1, figsize = (1.8, 1.8))
-    
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(1.8, 1.8))
+
     # Plot buggy version in first subplot
-    ax1.plot(timestamps_buggy, util_avg_values_buggy, linewidth=3, linestyle='-', color='#A72703')
+    ax1.plot(
+        timestamps_buggy,
+        util_avg_values_buggy,
+        linewidth=3,
+        linestyle="-",
+        color="#A72703",
+    )
     # ax1.set_xlabel('Time (ms)')
     ax1.set_ylabel("Avg Util")
     ax1.set_title("Buggy", fontsize=10, pad=3)
     ax1.grid(True, alpha=0.3)
     ax1.set_yticks([0, 1000])
-    ax1.set_yticklabels(['0', '1k'])
+    ax1.set_yticklabels(["0", "1k"])
     ax1.set_xticklabels([])
     ax1.set_xlim(0, max(max(timestamps_buggy), max(timestamps_fixed)))
-    
+
     # Plot fixed version in second subplot
-    ax2.plot(timestamps_fixed, util_avg_values_fixed, linewidth=3, linestyle='-', color='#BBC863')
-    ax2.set_xlabel('Time (ms)')
+    ax2.plot(
+        timestamps_fixed,
+        util_avg_values_fixed,
+        linewidth=3,
+        linestyle="-",
+        color="#BBC863",
+    )
+    ax2.set_xlabel("Time (ms)")
     ax2.set_ylabel("Avg Util")
     ax2.set_yticks([0, 1000])
-    ax2.set_yticklabels(['0', '1k'])
+    ax2.set_yticklabels(["0", "1k"])
     ax2.set_title("Fixed", fontsize=10, pad=3)
     ax2.grid(True, alpha=0.3)
     ax2.set_xlim(0, max(max(timestamps_buggy), max(timestamps_fixed)))
-    
+
     plt.tight_layout(pad=0.1)
-    plt.savefig(output_file)
+    return fig
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--controller", type=str, default="util_avg") # 17e3e88
+    parser.add_argument("--controller", type=str, default="util_avg")
     args = parser.parse_args()
 
-    bugId = args.controller
-    
     # Paths to the log files
-    buggy_log = RESULTS_DIR / f"{bugId}_buggy.log"
-    fixed_log = RESULTS_DIR / f"{bugId}_fixed.log"
-
-    output_file = RESULTS_DIR / f"{bugId}.pdf"
+    buggy_log = RESULTS_DIR / f"{args.controller}_buggy.log"
+    fixed_log = RESULTS_DIR / f"{args.controller}_fixed.log"
 
     timestamps_buggy, util_avg_values_buggy = parse_log_file(buggy_log)
     timestamps_fixed, util_avg_values_fixed = parse_log_file(fixed_log)
 
-    plot_util_avg(timestamps_buggy, util_avg_values_buggy, 
-                  timestamps_fixed, util_avg_values_fixed, bugId, output_file)
+    fig = plot_util_avg(
+        timestamps_buggy, util_avg_values_buggy, timestamps_fixed, util_avg_values_fixed
+    )
+    save_fig(fig, args.controller)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
-
--- scripts/plot_utils.py
+++ scripts/plot_utils.py
@@ -1,17 +1,22 @@
 from pathlib import Path
 
+from consts import RESULTS_DIR
 from matplotlib.figure import Figure
 
 
-def save_fig(fig: Figure, filename: Path):
-    fig.tight_layout()
-    pdf_path = filename.with_suffix(".pdf")
-    png_path = filename.with_suffix(".png")
+def save_fig(fig: Figure, filename: Path | str) -> list[Path]:
+    path = Path(filename) if isinstance(filename, str) else filename
+    path = path if path.is_absolute() else RESULTS_DIR / path
+    paths = [path.with_suffix(f".{ext}") for ext in ["pdf", "png"]]
 
-    fig.savefig(pdf_path, bbox_inches="tight", pad_inches=0, dpi=1000)
-    print(f"Saved figure to {pdf_path}")
-
-    fig.savefig(png_path, bbox_inches="tight", pad_inches=0, dpi=1000)
-    print(f"Saved figure to {png_path}")
-
-    return pdf_path, png_path
+    fig.tight_layout(pad=0)
+    for path in paths:
+        fig.savefig(
+            path,
+            bbox_inches="tight",
+            pad_inches=0,
+            dpi=1000,
+            metadata={"CreationDate": None},  # for reproducible builds
+        )
+        print(f"Saved figure to {path}")
+    return paths
