09e495a69101a012b08a3aa4ab99e4d01ee8a642
ShawnZhong <github@shawnzhong.com>
2026-01-29 20:35:09 +0000

    Update

--- kmod/task.c
+++ kmod/task.c
@@ -32,11 +32,14 @@ struct task_struct *kstep_task_create(void) {
   struct task_struct *p = NULL;
   struct subprocess_info *info = call_usermodehelper_setup(
       path, argv, NULL, GFP_KERNEL, task_init, NULL, &p);
-  if (info == NULL || p == NULL)
+  if (info == NULL)
     panic("Failed to setup user mode helper");
 
   if (call_usermodehelper_exec(info, UMH_WAIT_EXEC) < 0)
     panic("Failed to run user mode helper");
+
+  if (p == NULL)
+    panic("Failed to get task struct");
 
   // Wait for the task to start
   for (int i = 0; i < 100; i++) {
