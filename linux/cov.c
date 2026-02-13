f9d6e3330d164ce61cfe358cd14facc80078c607
Shawn Zhong <github@shawnzhong.com>
2026-02-12 23:36:38 -0600

    Reimpl cov (#249)
    * Reimpl cov
    
    * Update
    
    * Update
    
    * Update

---
+++ linux/cov.c
@@ -0,0 +1,12 @@
+#include <linux/export.h>
+
+void (*sanitizer_cov_trace_pc)(u64) = NULL;
+EXPORT_SYMBOL(sanitizer_cov_trace_pc);
+
+void notrace __sanitizer_cov_trace_pc(void);
+void notrace __sanitizer_cov_trace_pc(void)
+{
+	if (sanitizer_cov_trace_pc)
+          sanitizer_cov_trace_pc(__builtin_return_address(0));
+}
+EXPORT_SYMBOL(__sanitizer_cov_trace_pc);
