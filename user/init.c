d976844a11f25289d2dcbd244d987a7ae2d9793b
ShawnZhong <github@shawnzhong.com>
2026-02-15 18:15:47 -0600

    `set_tty_raw_output` in init

--- user/init.c
+++ user/init.c
@@ -5,6 +5,7 @@
 #include <sys/mount.h>   // mount
 #include <sys/stat.h>    // mkdir
 #include <sys/syscall.h> // SYS_*
+#include <termios.h>     // termios, tcgetattr, tcsetattr
 #include <unistd.h>      // open, close, syscall
 
 #include "utils.h"
@@ -40,6 +41,22 @@ static void load_kmod(const char *path, int argc, char *argv[], char *envp[]) {
   close(fd);
 }
 
+static void set_tty_raw_output(const char *path) {
+  int fd = open(path, O_RDWR | O_NOCTTY);
+  if (fd < 0)
+    panic("Failed to open %s", path);
+
+  struct termios termios;
+  if (tcgetattr(fd, &termios) < 0)
+    panic("Failed to tcgetattr %s", path);
+
+  termios.c_oflag &= ~(OPOST); // Disable output post-processing
+  if (tcsetattr(fd, TCSANOW, &termios) < 0)
+    panic("Failed to tcsetattr %s", path);
+
+  close(fd);
+}
+
 int main(int argc, char *argv[], char *envp[]) {
   mount_fs("/dev", "devtmpfs");
   mount_fs("/proc", "proc");
@@ -47,6 +64,7 @@ int main(int argc, char *argv[], char *envp[]) {
   mount_fs("/sys/kernel/debug", "debugfs");
   mount_fs("/sys/fs/cgroup", "cgroup2");
   set_proc_affinity(0, 0); // bind to cpu 0
+  set_tty_raw_output("/dev/ttyS2");
   load_kmod("kmod.ko", argc, argv, envp);
   panic("Kernel module exited unexpectedly");
 }
